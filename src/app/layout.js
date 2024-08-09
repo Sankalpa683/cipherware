c87fec6b4e2abb950c0f91061aed513fdce57453
Sankalpa Dahal <62802864+Sankalpa683@users.noreply.github.com>
2024-08-09 14:44:27 +0545

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "CipherWare",
-  description: `Welcome to CipherWareTo pay w/ CashApp, Apple Pay, Google Pay, or Venmo, open a ticket in our Discord server!
+  title: "CipherWAuth",
+  description: `Welcome to CipherAuthTo pay w/ CashApp, Apple Pay, Google Pay, or Venmo, open a ticket in our Discord server!
   Discord TikTok YouTube. 1784. Product Sold. 4.8.`,
 };
 
