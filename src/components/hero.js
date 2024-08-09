c54aa4f3466cdfeaf2c3722a885c613378d6b357
Sankalpa Dahal <62802864+Sankalpa683@users.noreply.github.com>
2024-08-09 14:26:22 +0545

    Update hero.js

--- src/components/hero.js
+++ src/components/hero.js
@@ -10,7 +10,6 @@ import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 export default function ShootingStarsAndStarsBackgroundDemo() {
   const words = ["CashApp", "Apple Pay", "Google Pay", "Venmo"];
-  const feedbacktexts = `Based on verified website purchases`;
 
   return (
     <section className="h-fit sm:h-[54rem] rounded-md bg-neutral-900 flex flex-col items-center pt-16 -pb-14 sm:py-20 px-4 relative w-full">
@@ -58,7 +57,7 @@ export default function ShootingStarsAndStarsBackgroundDemo() {
             {/* Removing AnimatedTooltip for performance */}
           </div>
           <div className="py-8">
-            <TextGenerateEffect words={feedbacktexts}/>
+            <p className="text-2xl">Based on verified website purchases</p>
             <div className="py-6">
               <button className='py-3 px-6 w-fit text-center bg-[#007BFF] text-[#ffffff] rounded-lg mt-2 transition-all hover:bg-blue-700'>
                 200+ Positive Feedbacks
