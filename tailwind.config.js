fc5022287f01d24ab157bee7ceeb5d2612528137
Sankalpa Dahal <62802864+Sankalpa683@users.noreply.github.com>
2024-08-11 16:43:40 +0545

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -69,7 +69,7 @@ module.exports = {
       },
       boxShadow: {
         'custom': 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px',
-        'nice': 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;'
+        'nice': 'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;',
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
